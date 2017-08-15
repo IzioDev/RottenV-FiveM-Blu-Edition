ee8f31cb7d229fcf8d048c0c0a464b2614449023
Blue <ericmager@gmx.de>
2017-08-15 20:15:35 +0200

    WHAT?!?! A WORKING DATABASE PROTOTYPE?

--- dbClient.lua
+++ dbClient.lua
@@ -1,27 +1,35 @@
 Citizen.CreateThread( function()
 	RegisterNetEvent('loadPlayerIn')
 	AddEventHandler('loadPlayerIn', function(x,y,z,hunger,thirst,weapons) 
+		Wait(500)
 		local playerPed = GetPlayerPed(-1)
-		SetEntityCoords(playerPed,x,y,z)
+		Citizen.Trace("Recieving Stats...")
+		SetEntityCoords(playerPed,x+.0,y+.0,z+.0,1,0,0,1)
 		weaponTable = {}
 		weaponTable.ammo = {}
 		index = 0
-		for value in string.gmatch(weapons, "|") do 
+		for _,value in ipairs(mysplit(weapons, "|")) do 
+			index = index + 1
 			weaponTable[index] = value
-			index = index + 1
+		--	Citizen.Trace("Got Gun:"..value)
+		
+		for _,value in ipairs(mysplit(value, ":")) do 
+			if not tonumber(value) then
+				weaponTable[index] = value
+			else
+				weaponTable.ammo[index] = value
+			end
 		end
-		index = 0
-		for value in string.gmatch(weapons, ":") do 
-			weaponTable.ammo[index] = value
-			index = index + 1
 		end
 		index = 0
 		for _,theWeapon in ipairs(weaponTable) do 
-			GiveWeaponToPed(ped, GetHashKey(theWeapon), weaponTable.ammo[index], true, true)
-			index = index +1
+		index = index +1
+		Citizen.Trace(weaponTable.ammo[index])
+			GiveWeaponToPed(playerPed, GetHashKey(theWeapon), weaponTable.ammo[index], true, true)
 		end
 		DecorSetFloat(playerPed, "hunger", hunger)
 		DecorSetFloat(playerPed, "thirst", thirst)
+		Citizen.Trace("Done!")
 	end)
 	
 	AddEventHandler("playerSpawned", function()
@@ -30,6 +38,24 @@ Citizen.CreateThread( function()
 		Citizen.Trace("Sent!")
 	end)
 	
+--	AddEventHandler("playerSpawned", function()
+--		TriggerServerEvent("spawnPlayer", GetPlayerServerId(PlayerId()))
+--		Citizen.Trace("Requesting Spawn!")
+--		Citizen.Trace("Sent!")
+--	end)
+	
+	
+function mysplit(inputstr, sep)
+        if sep == nil then
+                sep = "%s"
+        end
+        local t={} ; i=1
+        for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+                t[i] = str
+                i = i + 1
+        end
+        return t
+end
 	
 end)
 
--- dbServer.lua
+++ dbServer.lua
@@ -1,15 +1,19 @@
-
-Citizen.CreateThread(function()
 RegisterServerEvent('spawnPlayer')
-		AddEventHandler('spawnPlayer', function(player)
-			MySQL.Sync.fetchScalar('SELECT * FROM players where steamid="'..GetPlayerIdentifier(source,0)..'"', {}, function(player) 
-				if player[1].steamid == GetPlayerIdentifier(source,0) then
-					TriggerClientEvent("loadPlayerIn", source, player[1].x, player[1].y, player[1].z, player[1].hunger, player[1].thirst, player[1].weapons)
-					print("Client Requested Spawn!")
+Citizen.CreateThread(function()
+		AddEventHandler('spawnPlayer', function(client)
+		print("Client Requested Spawn!")
+			MySQL.Async.fetchAll('SELECT * FROM players where steamid="'..GetPlayerIdentifier(client,0)..'"', {}, function(player) 
+				print(player[1].steamid)
+				if player[1].steamid == GetPlayerIdentifier(client,0) then
+					TriggerClientEvent("loadPlayerIn", client, player[1].x, player[1].y, player[1].z, player[1].hunger, player[1].thirst, player[1].weapons)
 					print("Sent!")
 				end
 			end)
 	end)
+	
+		AddEventHandler("onPlayerWasted", function(client)
+		
+		end)
 end)
 
 
