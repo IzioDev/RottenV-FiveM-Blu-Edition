d27235287ba7797e9ae919b53504d704d1747196
Blue <ericmager@gmx.de>
2017-08-25 13:55:11 +0200

    fix a bug in DistancebetweenCoords

--- host.lua
+++ host.lua
@@ -4,7 +4,7 @@
 function DistanceBetweenCoords(x1,y1,z1,x2,y2,z2)
     local deltax = x1 - x2
     local deltay = y1 - y2
-    local deltaz = y1 - y2
+    local deltaz = z1 - z2
     
     dist = math.sqrt((deltax * deltax) + (deltay * deltay) + (deltaz * deltaz))
     xout = math.abs(deltax)
